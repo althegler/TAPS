eb209fe028ebca9a3181483973e5c1179fe0b7d5
Alberte Thegler <alberte@thegler.dk>
2018-05-23 22:31:09 +0200

    Added antlr examples

---
+++ ANTLR_example/chat/chat_py/ChatListener.py
@@ -0,0 +1,87 @@
+# Generated from Chat.g4 by ANTLR 4.7.1
+from antlr4 import *
+
+# This class defines a complete listener for a parse tree produced by ChatParser.
+class ChatListener(ParseTreeListener):
+
+    # Enter a parse tree produced by ChatParser#chat.
+    def enterChat(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#chat.
+    def exitChat(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#line.
+    def enterLine(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#line.
+    def exitLine(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#message.
+    def enterMessage(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#message.
+    def exitMessage(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#name.
+    def enterName(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#name.
+    def exitName(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#command.
+    def enterCommand(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#command.
+    def exitCommand(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#emoticon.
+    def enterEmoticon(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#emoticon.
+    def exitEmoticon(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#link.
+    def enterLink(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#link.
+    def exitLink(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#color.
+    def enterColor(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#color.
+    def exitColor(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by ChatParser#mention.
+    def enterMention(self, ctx):
+        pass
+
+    # Exit a parse tree produced by ChatParser#mention.
+    def exitMention(self, ctx):
+        pass
+
+
