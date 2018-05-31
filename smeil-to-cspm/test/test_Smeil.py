f10fad707db44f966f6d9054963d00c41fa3f99c
althegler <alberte@thegler.dk>
2018-05-31 17:03:34 +0200

    Created tests for channels

--- smeil-to-cspm/test/test_Smeil.py
+++ smeil-to-cspm/test/test_Smeil.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('/home/albt/git/SMEIL-to-CSPm/smeil-to-cspm/')
+sys.path.append('/home/alt/git/SMEIL-to-CSPm/smeil-to-cspm/')
+# sys.path.append('/home/albt/git/SMEIL-to-CSPm/smeil-to-cspm/')
 
 from antlr4 import *
 from SmeilLexer import SmeilLexer
@@ -18,9 +19,9 @@ def test_proc_name():
     printer = SmeilCspmListener()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
-    output = "Clock = "
+    test_output = "Clock = "
 
-    assert printer.get_process() == output
+    assert printer.get_process() == test_output
 
 
 def test_variables():
@@ -33,6 +34,24 @@ def test_variables():
     printer = SmeilCspmListener()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
-    output = "Clock = \n\tHour : "
+    test_output = "Clock = \n\thour : "
 
-    assert printer.get_process() == output
+    assert printer.get_process() == test_output
+
+
+def test_channel():
+    test_input = "proc clock()\n\tbus clock_out {\n\tval: int;\n\t};\n\tvar hour : int ;"
+    lexer = SmeilLexer(InputStream(test_input))
+    stream = CommonTokenStream(lexer)
+    parser = SmeilParser(stream)
+    tree = parser.process()
+
+    printer = SmeilCspmListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+    test_output = "channel clock_out_val : {42} \n\nClock = \n\thour : "
+    output = printer.get_channel() + printer.get_process()
+    print test_output
+    print output
+
+    assert (output == test_output)
