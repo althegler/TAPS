7670e3859b777db968440fbffad7b4866f555123
Alberte Thegler <alberte@thegler.dk>
2018-06-03 15:07:17 +0200

    Added test for process variables

--- smeil-to-cspm/test/test_Smeil.py
+++ smeil-to-cspm/test/test_Smeil.py
@@ -51,8 +51,6 @@ def test_channel():
     walker.walk(printer, tree)
     test_output = "channel clock_out_val : {42} \n\nClock = \n\thour : "
     output = printer.get_channel() + printer.get_process()
-    print test_output
-    print output
 
     assert output == test_output
 
@@ -69,9 +67,22 @@ def test_several_channels():
     walker.walk(printer, tree)
     test_output = "channel clock_out_val1 : {42} \n\nchannel clock_out_val2 : {42} \n\nClock = \n\thour : "
     output = printer.get_channel() + printer.get_process()
-    print test_output
-    print output
 
+    assert output == test_output
+
+def test_process_input_variable():
+    test_input = "proc clock( in hours_in )"
+    lexer = SmeilLexer(InputStream(test_input))
+    stream = CommonTokenStream(lexer)
+    parser = SmeilParser(stream)
+    tree = parser.process()
+
+    printer = SmeilCspmListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+    test_output = "Clock(hours_in) = "
+    output = printer.get_channel() + printer.get_process()
+    print output
     assert output == test_output
 
 
