5376523b5153229f24032fc452cf02700ed4ab52
Alberte Thegler <alberte@thegler.dk>
2018-06-05 16:26:54 +0200

    Fixed tests to match new implementation

--- smeil-to-cspm/test/test_Smeil.py
+++ smeil-to-cspm/test/test_Smeil.py
@@ -12,12 +12,12 @@ import pytest
 
 def test_proc_name():
     test_input = "proc clock(in x) { }"
-    test_output = "Clock(x) = \n\t"
+    test_output = "Clock(x) = \n"
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -26,12 +26,12 @@ def test_proc_name():
 
 def test_variables():
     test_input = "proc clock(in x)var hour : int ; { }"
-    test_output = "Clock(x) = \n\t"
+    test_output = "Clock(x) = \n"
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -41,31 +41,31 @@ def test_variables():
 
 
 def test_channel():
-    test_input = "proc clock(in x) bus clock_out {val: int;};var hour : int ; { }"
-    test_output = "channel clock_out_val : {42} \n\nClock(x) = \n\t"
+    test_input = "proc clock(in x) bus clock_out {val: int range 0 to 2;};var hour : int ; { }"
+    test_output = "channel clock_out_val : {0..2} \nClock(x) = \n"
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
-    # print output
-    # print test_output
+    print output
+    print test_output
     assert output == test_output
 
 
 def test_several_channels():
-    test_input = ("proc clock( in x) bus clock_out {val1: int; val2: int;}; " +
-                  "var hour : int ; { }")
-    test_output = ("channel clock_out_val1 : {42} " +
-                  "\n\nchannel clock_out_val2 : {42} \n\nClock(x) = \n\t")
+    test_input = ("proc clock( in x) bus clock_out {val1: int range 0 to 1; val2: int range 0 to 3;}; " +
+                  "var hour : int ; {}")
+    test_output = ("channel clock_out_val1 : {0..1} " +
+                  "\nchannel clock_out_val2 : {0..3} \nClock(x) = \n")
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -80,7 +80,7 @@ def test_process_input_variable():
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -89,13 +89,13 @@ def test_process_input_variable():
     assert output == test_output
 
 def test_process_empty_statement():
-    test_input = "proc clock(in x) bus clock_out {val: int;}; var hour : int ; { }"
-    test_output = "channel clock_out_val : {42} \n\nClock(x) = \n\t"
+    test_input = "proc clock(in x) bus clock_out {val: int range 10 to 20;}; var hour : int ; { }"
+    test_output = "channel clock_out_val : {10..20} \nClock(x) = \n"
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -104,15 +104,15 @@ def test_process_empty_statement():
     assert output == test_output
 
 def test_process_statement():
-    test_input = ("proc clock(in x) bus clock_out {val: int;}; var hour : int ;" +
+    test_input = ("proc clock(in x) bus clock_out {val: int range 42 to 43;}; var hour : int ;" +
                   " { minutes_first_temp = 10; }")
-    test_output = ("channel clock_out_val : {42} \n\nClock(x) = \n\t" +
-                   "let\n\t\tminutes_first_temp = 10\n\twithin\n\t\t")
+    test_output = ("channel clock_out_val : {42..43} \nClock(x) = \n" +
+                   "let\nminutes_first_temp = 10\nwithin\n")
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
@@ -121,16 +121,16 @@ def test_process_statement():
     assert output == test_output
 
 def test_process_communication():
-    test_input = ("proc clock( in x) bus clock_out {val: int;}; var hour : int ;" +
+    test_input = ("proc clock( in x) bus clock_out {val: int range 4 to 6;}; var hour : int ;" +
                   " { minutes = 10; clock_out.val = minutes;}")
-    test_output = ("channel clock_out_val : {42} \n\nClock(x) = \n\t" +
-                   "let\n\t\tminutes = 10\n\twithin\n\t\t" +
+    test_output = ("channel clock_out_val : {4..6} \n\nClock(x) = \n" +
+                   "let\nminutes = 10\nwithin\n" +
                    "clock_out_val ! minutes -> \nSKIP")
     lexer = SmeilLexer(InputStream(test_input))
     stream = CommonTokenStream(lexer)
     parser = SmeilParser(stream)
     tree = parser.process()
-    printer = SmeilCspmListener()
+    printer = SmeilCspmListenerVersion2()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     output = printer.get_channel() + printer.get_process()
