b4aedd8717a107543fe47bd014e3f5670caf793f
Alberte Thegler <alberte@thegler.dk>
2018-06-05 15:54:09 +0200

    Added new version of listener. Needed to restructure code

---
+++ smeil-to-cspm/SmeilCspmListenerVersion2.py
@@ -0,0 +1,95 @@
+from antlr4 import *
+from SmeilLexer import SmeilLexer
+from SmeilListener import SmeilListener
+from SmeilParser import SmeilParser
+import sys
+
+class SmeilCspmListenerVersion2(SmeilListener) :
+    def __init__(self):
+        self.process = ''
+        self.network = ''
+        self.channel = ''
+
+    def get_process(self):
+        return self.process
+
+    def get_network(self):
+        return self.network
+
+    def get_channel(self):
+        return self.channel
+
+    def exitProcess(self, ctx):
+        # There will always be a process name
+        self.process += ctx.ident().getText().capitalize()
+        # print ctx.children
+        params_val = next((x.getText() for x in ctx.children if isinstance(x, SmeilParser.ParamsContext)), None)
+        if params_val == None :
+            # If there is no params we want to create an input channels
+            print "no params"
+        else:
+            self.process += '('
+            self.process += ctx.params().text
+            self.process += ')'
+            self.process += ' = \n'
+            self.process += 'let\n'
+            self.process += let_variables
+            self.process += 'within\n'
+            if len(within_variables) > 1:
+                within_variables += 'SKIP'
+            self.process += within_variables
+
+
+
+
+    def exitDeclaration(self, ctx):
+        for child in ctx.children:
+            if isinstance(child, SmeilParser.BusdeclContext) is True:
+                self.channel += child.text
+            elif isinstance(child, SmeilParser.VardeclContext) is True:
+                print "VardeclContext"
+                # print child.text
+
+    def exitBusdecl(self, ctx):
+        text = ''
+        bus_name = ctx.ident().getText() + '_'
+        # get all channel declarations within the bus declaration
+        # in order to create all channels
+        for child in ctx.bussignaldecls().children:
+            channel_name = child.ident().getText()
+            range = '{'
+            ranges = []
+            for expression in child.ranges().children:
+                if hasattr(expression, 'text'):
+                    ranges.append(expression.text)
+            if len(ranges) > 2:
+                print "ERROR: not allowed more than two expressions in range"
+            else:
+                range += ranges[0] + '..' + ranges[1] + '}'
+            text += ('channel ' + bus_name + channel_name + ' : ' + range +'\n')
+        ctx.text = text
+
+
+    def exitExpression(self,ctx):
+        #TODO can only handle one child currently
+        for child in ctx.children:
+            ctx.text = child.text
+
+
+    def exitName(self, ctx):
+        if ctx.ident():
+            ctx.text = ctx.ident().getText()
+        else:
+            # TODO what if it is not an ident?
+            print ctx.children
+
+    def enterParams(self, ctx):
+        text = ''
+        for child in ctx.children:
+            if isinstance(child, SmeilParser.ParamContext) is True:
+                text += child.ident().getText()
+                if child != ctx.children[-1]:
+                    text += ', '
+        ctx.text = text
+
+
