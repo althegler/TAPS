b3b6d64d865a6ca608f7ba6d5c7cabb907e36771
Alberte Thegler <alberte@thegler.dk>
2018-08-12 21:40:47 +0200

    Finished monitor data transformations

--- smeil-to-cspm/SmeilToCSPm.py
+++ smeil-to-cspm/SmeilToCSPm.py
@@ -29,7 +29,9 @@ def main():
     # walker.walk(printer, tree)
 
     # Transformation
-    result = transform_channels(data)
+    # result = transform_channels(data)
+    result = transform_monitor(data)
+    print result
     # Load
     # output = open("output.csp","w")
     # output.write(printer.get_channel())
@@ -55,12 +57,37 @@ def transform_channels(data):
                 cspm_channel_name = (process_name + '_'
                                   + bus_name + '_'
                                   + channel['channel_name'])
-                cspm_channel_bounds = (channel['lowerbound'],
-                                       channel['upperbound'])
-                channels[cspm_channel_name] = (cspm_channel_bounds)
-                # print channel['channel_name']
-    print channels
+                bounds = max_bits(channel['type'])
+                channels[cspm_channel_name] = bounds
     return channels
+
+def transform_monitor(data):
+    monitors = {}
+    input_channels = []
+    for instance in data['network']:
+        if instance['instance_input'] == None:
+            # Find input channels
+            input_channels.append(instance['proc_name'])
+    for process_name, process in data['channels'].iteritems():
+        if process_name not in input_channels:
+            # Ignore processes without input bus
+            for bus_name, bus in process.iteritems():
+                for channel in bus:
+                    cspm_monitor_name = (process_name + '_'
+                                      + bus_name + '_'
+                                      + channel['channel_name']
+                                      + '_monitor')
+                    monitors[cspm_monitor_name] = (channel['lowerbound'],
+                                                   channel['upperbound'])
+    return monitors
+
+def max_bits(b):
+    sign = b[0]
+    bit = int(b[1:])
+    if sign == 'i':
+        return (-((2**(bit-1)) - 1), ((2**(bit-1)) - 1))
+    else:
+        return (0,(2 ** bit) - 1)
 
 
 if __name__ == '__main__':
