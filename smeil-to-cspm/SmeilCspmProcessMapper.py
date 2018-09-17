c9b1d0be29c71deb1cc5f681dd2d6fec4afb8696
althegler <alberte@thegler.dk>
2018-09-17 15:53:45 +0200

    Added stack functionality to process mapper

--- smeil-to-cspm/SmeilCspmProcessMapper.py
+++ smeil-to-cspm/SmeilCspmProcessMapper.py
@@ -7,84 +7,77 @@ import sys
 class SmeilCspmProcessMapper(SmeilListener):
     def __init__(self, data):
         self.data = data
+        self.stack = []
 
     def exitProcess(self, ctx):
         communication_list = []
         calculations_list = []
         process = {}
-        proc_name = ctx.IDENT().getText()
+        # TODO: Note that we are only interested in the input bus, and therefore
+        # we are not looking at the other two potential input
         proc_input_variable = next((x.getText()
                               for x in ctx.children
                               if isinstance(x, SmeilParser.ParamsContext)),
                               None)
         if proc_input_variable != None:
-            proc_input_variable = ctx.params().text
+            process['input_value'] = self.stack.pop()
         for child in ctx.statement():
             if hasattr(child, 'communication'):
                 communication_list.append(child.communication)
             else:
                 calculations_list.append(child.calculation)
-        # process['proc_name'] = proc_name
-        process['input_value'] = proc_input_variable
         process['calculations_list'] = calculations_list
         process['communication_list'] = communication_list
-
-        self.data['processes'][proc_name] = process
+        self.data['processes'][ctx.IDENT().getText()] = process
 
     def exitStatement(self, ctx):
-        # TODO currently only handling "name = expression ;"
+        # TODO currently only handling "name = expression ;" and trace(/nothing)
         # TODO what happens with several expressions in one?
         formatstring = next((x.getText() for x in ctx.children if
             isinstance(x, SmeilParser.FormatstringContext)), None)
         ## If the statement is a trace, it does nothing.
         if formatstring == None:
-            expression = next((x.text for x in ctx.children if
-                isinstance(x, SmeilParser.ExpressionContext)), None)
-            if expression != None:
-                if hasattr(ctx.name(), 'comm_text'):
-                    # # Communication
-                    ctx.communication = (ctx.name().comm_text, expression)
-                else:
-                    # # Calculations
-                    ctx.calculation = ctx.getText()
+            expression = self.stack.pop()
+            name = self.stack.pop()
+            if '.' in name:
+                # # Communication
+                ctx.communication = (name, expression)
+            else:
+                # # Calculations
+                ctx.calculation = (name, expression)
 
 
     def exitExpression(self,ctx):
-        #TODO can only handle one child currently
-        text = ''
+        text = []
         if ctx.getChildCount() > 1:
-            expression_text = ''
-            for child in ctx.children:
-                expression_text += child.text
-                expression_text += ' '
-            text = expression_text
-        else:
-            text += ctx.children[0].text
-        ctx.text = text
+            for _ in ctx.children:
+                text.append(self.stack.pop())
+            self.stack.append(list(reversed(text)))
 
     def exitBinop(self, ctx):
-        ctx.text = ctx.getText()
+        self.stack.append(ctx.getText())
 
     def exitLiteral(self,ctx):
-        ctx.text = ctx.INTEGER().getText()
+        self.stack.append(ctx.getText())
 
     def exitName(self, ctx):
         # TODO can only handle part of the grammar
         # TODO Is it possible to make the parser parse one level down by itself
         # instead of me doing ctx.name()[0].getText()? Are there a better way?
         if ctx.getChildCount() > 1:
-            ctx.comm_text = ctx.getText()
-        else:
-            if ctx.IDENT():
-                ctx.text = ctx.IDENT().getText()
-            else:
-                ctx.text = ctx.NUM().getText()
+            ctx.comm_text = True
+        self.stack.append(ctx.getText())
 
-    def enterParams(self, ctx):
-        text = ''
+    def exitParams(self, ctx):
+        # TODO: I need to be able to handle out or const params, but currently I only
+        # need the in parameter.
         for child in ctx.children:
             if isinstance(child, SmeilParser.ParamContext) is True:
-                text += child.IDENT().getText()
-                if child != ctx.children[-1]:
-                    text += ', '
-        ctx.text = text
+                param = self.stack.pop()
+        # TODO: The last parameter popped is the input parameter in our case.
+        # This is not necessaryly always the case, so something should be done
+        # about this solution
+        self.stack.append(param)
+
+    def exitParam(self, ctx):
+        self.stack.append(ctx.IDENT().getText())
