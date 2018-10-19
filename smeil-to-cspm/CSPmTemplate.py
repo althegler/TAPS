b198a430c5abdc92cce2d3b688b5d8d0a536a2b7
Alberte Thegler <alberte@thegler.dk>
2018-10-19 20:16:00 +0200

    Added testing environment

--- smeil-to-cspm/CSPmTemplate.py
+++ smeil-to-cspm/CSPmTemplate.py
@@ -5,7 +5,14 @@ from jinja2 import Environment, FileSystemLoader
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-j_env = Environment(loader=FileSystemLoader(THIS_DIR + "/templates"), trim_blocks=True)
+j_env = Environment(loader=FileSystemLoader(THIS_DIR + "/templates"),
+        trim_blocks=True, lstrip_blocks=True)
+j_test_env = Environment(loader=FileSystemLoader(THIS_DIR + "/templates/minimal"),
+             trim_blocks=True, lstrip_blocks=True)
 
 def templating(data):
     return j_env.get_template('cspm.j2').render(data=data)
+
+
+def test_templating(data):
+    return j_test_env.get_template('cspm.j2').render(data=data)
