3e63fc1836c73ef75d17612f576b8ae6e57de253
Alberte Thegler <alberte@thegler.dk>
2018-11-28 20:58:12 +0100

    Cleaned up

--- tests/test_Smeil.py
+++ tests/test_Smeil.py
@@ -34,99 +34,43 @@ def test_proc_variable_in_process():
 
 def test_proc_output_bus():
     test_input = "proc clock() \
-                    bus clock_out { \
+                    bus outbus { \
                         val: u4 range 0 to 2; \
                     }; \
                     { }"
-    test_output = "channel clock_clock_out_val : {0 .. 15}\nclock_clock_out_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
+    test_output = "channel clock_outbus_val : {0 .. 15}\nclock_outbus_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
     output = taps(test_input)
     assert output == test_output
 
 def test_proc_output_bus_several_channels():
     test_input = "proc clock() \
-                    bus clock_out { \
+                    bus outbus { \
                         val: u4 range 0 to 2; \
                         val2: u4 range 0 to 2; \
                     }; \
                     { }"
-    test_output = "channel clock_clock_out_val : {0 .. 15}\nchannel clock_clock_out_val2 : {0 .. 15}\nclock_clock_out_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\nclock_clock_out_val2_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
+    test_output = "channel clock_outbus_val2 : {0 .. 15}\nchannel clock_outbus_val : {0 .. 15}\nclock_outbus_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\nclock_outbus_val2_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
+    output = taps(test_input)
+    assert output == test_output
+
+def test_proc_statement_no_input():
+    test_input = "proc clock() \
+                    bus outbus { \
+                        val: u4 range 0 to 2; \
+                    }; \
+                    { outbus.val = 1; }"
+    test_output = "channel clock_outbus_val : {0 .. 15}\nclock_outbus_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
     output = taps(test_input)
     assert output == test_output
 
 
-#
-# #TODO this should not be a valid process definition. it has no communication
-# # but still creates an assertion process
-# def test_process_statement():
-#     test_input = ("proc clock(in x) bus clock_out {val: u4 range 42 to 43;}; var hour : u4 ;" +
-#                   " { minutes_first_temp = 10; }")
-#     test_output = ("channel clock_out_val : {42..43} \n\nClock(x) = \n" +
-#                    "let\nminutes_first_temp = 10\nwithin\n\n\n" +
-#                    "clock_out_val_assert(c) = c ? x -> if 42 <= x <= 43 then STOP else SKIP\n\n\n")
-#     lexer = SmeilLexer(InputStream(test_input))
-#     stream = CommonTokenStream(lexer)
-#     parser = SmeilParser(stream)
-#     tree = parser.module()
-#     printer = SmeilCspmListenerVersion2()
-#     walker = ParseTreeWalker()
-#     walker.walk(printer, tree)
-#     output = printer.get_channel() + printer.get_process()
-#     print output
-#     print test_output
-#     assert output == test_output
-#
-# # NOTE the same as assertion process (I realised it when I added those)
-# # Right not is asserts that it is not equal
-# # def test_process_communication():
-# #     test_input = ("proc clock( in x) bus clock_out {val: u4 range 4 to 6;}; var hour : u4 ;" +
-# #                   " { minutes = 10; clock_out.val = minutes;}")
-# #     test_output = ("channel clock_out_val : {4..6} \n\nClock(x) = \n" +
-# #                    "let\nminutes = 10\nwithin\n" +
-# #                    "clock_out_val ! minutes -> \nSKIP\n\n")
-# #     lexer = SmeilLexer(InputStream(test_input))
-# #     stream = CommonTokenStream(lexer)
-# #     parser = SmeilParser(stream)
-# #     tree = parser.module()
-# #     printer = SmeilCspmListenerVersion2()
-# #     walker = ParseTreeWalker()
-# #     walker.walk(printer, tree)
-# #     output = printer.get_channel() + printer.get_process()
-# #     print output
-# #     print test_output
-# #     assert output != test_output
-#
-# def test_input_process():
-#     test_input = ("proc clock() bus clock_out {val: u4 range 4 to 6;}; var hour : u4 ;" +
-#                   " { minutes = 10; clock_out.val = minutes;}")
-#     test_output = ("channel clock_out_val : {4..6} \n\n")
-#     lexer = SmeilLexer(InputStream(test_input))
-#     stream = CommonTokenStream(lexer)
-#     parser = SmeilParser(stream)
-#     tree = parser.module()
-#     printer = SmeilCspmListenerVersion2()
-#     walker = ParseTreeWalker()
-#     walker.walk(printer, tree)
-#     output = printer.get_channel() + printer.get_process()
-#     print output
-#     print test_output
-#     assert output == test_output
-#
-# def test_assertion_process():
-#     test_input = ("proc clock( in x) bus clock_out {val: u4 range 4 to 6;}; var hour : u4 ;" +
-#                   " { minutes = 10; clock_out.val = minutes;}")
-#     test_output = ("channel clock_out_val : {4..6} \n\nClock(x) = \n" +
-#                    "let\nminutes = 10\nwithin\n" +
-#                    "clock_out_val ! minutes -> \nSKIP\n\n" +
-#                    "clock_out_val_assert(c) = c ? x -> if 4 <= x <= 6 then STOP else SKIP\n\n\n")
-#     lexer = SmeilLexer(InputStream(test_input))
-#     stream = CommonTokenStream(lexer)
-#     parser = SmeilParser(stream)
-#     tree = parser.module()
-#     printer = SmeilCspmListenerVersion2()
-#     walker = ParseTreeWalker()
-#     walker.walk(printer, tree)
-#     output = printer.get_channel() + printer.get_process()
-#     print output
-#     print test_output
-#     assert output == test_output
-#
+def test_proc_statement_with_input():
+    test_input = "proc clock( in inbus) \
+                    bus outbus { \
+                        val: u4 range 0 to 2; \
+                    }; \
+                    { result = inbus.val + 1; \
+                    outbus.val = result; }"
+    test_output = "channel clock_outbus_val : {0 .. 15}\nClock(inbus) =\nlet\nresult = inbus + 1\nwithin\nclock_outbus_val ! result ->\nSKIP\nclock_outbus_val_monitor(c) = c ? x -> if 0 <= x and x <= 2 then SKIP else STOP\n"
+    output = taps(test_input)
+    assert output == test_output
